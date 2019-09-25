a0bbd86020903030fee648b926b8446262977442
Camilo Javier Olivo Sierra <camilo.olivo1990@gmail.com>
2019-09-24 21:05:23 -0500

    Se rralizan cambios en el css y se conecta con el servidor

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
+  private client: Client;
   constructor() { }
 
   ngOnInit() {
+    this.client = new Client();
+    this.client.webSocketFactory = ()=>{
+      return new SockJS("http://localhost:8080/chat-websocket");
+    }
+
+    this.client.onConnect = (frame) => {
+      console.log('Conectados: ' +  this.client.connected + ' : ' +  frame);
+    }
+    this.client.activate();
   }
 
 }
